e4b372f6e5446a8d5b19ba6710fb213356ea29b8
khalid <mdmahfuzbd@gmail.com>
2021-04-01 03:01:28 +0600

    api 2 gets,1 post,1 delete added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const ObjectId = require('mongodb').ObjectID; //this is needed to delete from mongodb or get single object
 
 require('dotenv').config();
 const dbName = process.env.DB_NAME;
@@ -10,30 +11,80 @@ const dbPass = process.env.DB_PASS;
 const port = process.env.PORT || 5050;
 
 app.use(express.json());
+app.use(express.urlencoded()); // need this for getting form data
 app.use(cors());
 
-app.listen(port,()=>{
-    console.log("I am listening on port : ",port);
+app.listen(port, () => {
+    console.log("I am listening on port : ", port);
 })
 
 //general api not database related. 
-app.get('/',(req, res)=>{
+app.get('/', (req, res) => {
     res.send("This is the server root, Howdy")
 })
 
+app.get('/testadd',(req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
 
 //mongodb connection
 const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${dbUser}:${dbPass}@cluster0.lroqv.mongodb.net/${dbName}?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true ,connectTimeoutMS: 30000, keepAlive: 1 });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, connectTimeoutMS: 30000, keepAlive: 1 });
 
 //api for mongodb related collections
 client.connect(err => {
     console.log(err);
     console.log("mongodb connected: OK");
-  const fruitsCollection = client.db("desifruits").collection("fruits");
-  // perform actions on the collection object
+    const fruitsCollection = client.db("desifruits").collection("fruits");
 
-  //client.close();
+    // perform actions on the collection object
+
+    //GET : get all fruits from fruits collection objects
+    app.get('/allFruits', (req, res) => {
+        fruitsCollection.find({}).toArray()
+            .then((items) => {
+                console.log("All-Fruits: ", items);
+                res.send(items);
+            })
+    })
+
+    //GET : get single fruit from database
+    app.get('/fruit/:id', (req, res) => {
+        const objId = req.params.id;
+        fruitsCollection.find({ _id: ObjectId(objId) }).toArray()
+            .then((doc) => {
+                res.send(doc[0]);
+            })
+    })
+
+
+    //POST : add one fruit to fruits collection objects
+    app.post('/addFruit', (req, res) => {
+        const newFruit = req.body;
+        console.log(newFruit)
+        fruitsCollection.insertOne(newFruit)
+            .then(result => {
+                res.send(result.insertedCount > 0);
+                console.log(result.insertedCount);
+            })
+    })
+
+
+
+    //DELETE : delete one fruit from fruits collection objects
+    app.delete('/delete/:id', (req, res) => {
+        const objId = req.params.id;
+        fruitsCollection.deleteOne({ _id: ObjectId(objId) })
+            .then((result) => {
+                // console.log(result);
+                res.send(result.deletedCount > 0); //delete is successfull 
+            })
+        //console.log(req.params.id)
+    })
+
+    //Others TODO:
+
+    //client.close();
 });
 
